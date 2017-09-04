9eba80b32cee1d61970cbf2b7e0025ad8b3ae10d
Biruntha <birunthagnanes@gmail.com>
2017-09-04 10:18:41 +0530

    version change

--- src/test/java/org/wso2/carbon/connector/integration/test/github/GithubConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/github/GithubConnectorIntegrationTest.java
@@ -39,7 +39,7 @@ public class GithubConnectorIntegrationTest extends ConnectorIntegrationTestBase
 
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
-        init("github-connector-1.0.2-SNAPSHOT");
+        init("github-connector-1.0.2");
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
         apiRequestHeadersMap.put("Accept-Charset", "UTF-8");
